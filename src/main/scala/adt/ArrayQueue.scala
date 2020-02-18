bf142560e2d7d77696c1c5593081f7b6ba9b4a5e
Amber Carlson <acarlso1@trinity.edu>
2020-02-18 11:10:11 -0600

    added maze solver code

--- src/main/scala/adt/ArrayQueue.scala
+++ src/main/scala/adt/ArrayQueue.scala
@@ -2,14 +2,20 @@ package adt
 
 import scala.reflect.ClassTag
 
-class ArrayQueue[A] extends Queue[A] {
+class ArrayQueue[A: ClassTag] extends Queue[A] {
   private var data = Array.fill(10)(null.asInstanceOf[A])
   private var front = 0
   private var back = 0
 
+  def dequeue(): A = {
+    val ret = data(front)
+    front = (front + 1) % data.length
+    ret
+  }
+
   def enqueue(a: A): Unit = {
     if ((back + 1) % data.length == front) {
-      val tmp = Array.fill(2 * data.length)(null.asInstanceOf[A])
+      val tmp = Array.fill(data.length * 2)(null.asInstanceOf[A])
       for (i <- 0 until data.length - 1) {
         tmp(i) = data((front + i) % data.length)
       }
@@ -20,12 +26,8 @@ class ArrayQueue[A] extends Queue[A] {
     data(back) = a
     back = (back + 1) % data.length
   }
-  def dequeue(): A = {
-    val ret = data(front)
-    front = (front + 1) % data.length
-    ret
-  }
-  def peek: A = data(front)
-  def isEmpty: Boolean = front == back
 
+  def peek: A = data(front)
+
+  def isEmpty: Boolean = front == back
 }
--- src/main/scala/basics/ShortestPath.scala
+++ src/main/scala/basics/ShortestPath.scala
@@ -1,16 +1,49 @@
 package basics
 
+import collection.mutable
+
+/**
+  * Changes to make for graphics game:
+  * Int to Double
+  * wall check becomes isClear
+  * check for start location being in a wall
+  * check for isClear covers checking for out of bounds
+  */
 object shortestPath {
-    val maze = Array(
-        Array(0,1,0,0,0,0,0,0,0,0),
-        Array(0,1,0,1,1,0,0,0,0,0),
-        Array(0,1,0,0,0,0,0,0,0,0),
-        Array(0,1,1,1,0,0,0,0,0,0),
-        Array(0,1,0,0,0,0,0,0,0,0),
-        Array(0,0,0,0,0,0,0,0,0,0),
-        Array(0,1,1,1,1,1,0,0,0,0),
-        Array(0,1,0,0,0,0,0,0,0,0),
-        Array(0,1,0,0,0,0,0,0,0,0),
-        Array(0,0,0,0,0,0,0,0,0,0),
-    )
+  val maze = Array(
+    Array(0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+    Array(0, 1, 0, 1, 1, 1, 1, 1, 1, 0),
+    Array(0, 1, 0, 0, 0, 1, 0, 0, 0, 0),
+    Array(0, 1, 1, 1, 0, 1, 0, 1, 1, 1),
+    Array(0, 1, 0, 0, 0, 1, 0, 0, 0, 0),
+    Array(0, 0, 0, 0, 0, 1, 0, 1, 1, 0),
+    Array(0, 1, 1, 1, 1, 1, 0, 0, 0, 0),
+    Array(0, 1, 0, 0, 0, 1, 1, 0, 1, 0),
+    Array(0, 1, 0, 1, 0, 1, 0, 0, 1, 0),
+    Array(0, 0, 0, 1, 0, 0, 0, 0, 1, 0)
+  )
+
+  val offsets = Array((-1, 0), (1, 0), (0, -1), (0, 1))
+  def breadthFirstShortestPath(sx: Int, sy: Int, ex: Int, ey: Int): Int = {
+    val queue = new ArrayQueue[(Inc, Int, Int)]()
+    val visited = mutable.Set[(Int, Int)]((sx, sy))
+    queue.enqueue((sx, sy, 0))
+    while (!queue.isEmpty) {
+      val (x, y, steps) = queue.dequeue()
+      for ((offsetx, offsety) <- offsets) {
+        val nx = x + offsetxval
+        val ny = y + offsetyval
+        if (nx == ex && ny == ey) return steps + 1
+        if (nx >= 0 && nx < maze.length && ny >= 0 && ny < maze(nx).length && maze(nx)(ny) == 0  && !visited((nx, ny))) {
+          queue.enqueue((nx, ny, steps + 1))
+          visited += nx -> ny
+        }
+      }
+    }
+    1000000000
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(breadthFirstshortestPath(0, 0, 9, 9))
+  }
 }
