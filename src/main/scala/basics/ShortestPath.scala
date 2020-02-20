ab2ee61969b6c23feea786656f493484abc352aa
Amber Carlson <acarlso1@trinity.edu>
2020-02-20 11:03:26 -0600

    added code for mandlebrot set

--- src/main/scala/basics/ShortestPath.scala
+++ src/main/scala/basics/ShortestPath.scala
@@ -1,40 +1,35 @@
 package basics
 
+import adt.ArrayQueue
 import collection.mutable
 
-/**
-  * Changes to make for graphics game:
-  * Int to Double
-  * wall check becomes isClear
-  * check for start location being in a wall
-  * check for isClear covers checking for out of bounds
-  */
-object shortestPath {
+object ShortestPath {
   val maze = Array(
-    Array(0, 1, 0, 0, 0, 0, 0, 0, 0, 0),
-    Array(0, 1, 0, 1, 1, 1, 1, 1, 1, 0),
-    Array(0, 1, 0, 0, 0, 1, 0, 0, 0, 0),
-    Array(0, 1, 1, 1, 0, 1, 0, 1, 1, 1),
-    Array(0, 1, 0, 0, 0, 1, 0, 0, 0, 0),
-    Array(0, 0, 0, 0, 0, 1, 0, 1, 1, 0),
-    Array(0, 1, 1, 1, 1, 1, 0, 0, 0, 0),
-    Array(0, 1, 0, 0, 0, 1, 1, 0, 1, 0),
-    Array(0, 1, 0, 1, 0, 1, 0, 0, 1, 0),
-    Array(0, 0, 0, 1, 0, 0, 0, 0, 1, 0)
+    Array(0,1,0,0,0,0,0,0,0,0),
+    Array(0,1,0,1,1,1,1,1,1,0),
+    Array(0,1,0,0,0,1,0,0,0,0),
+    Array(0,1,1,1,0,1,0,1,1,1),
+    Array(0,1,0,0,0,1,0,0,0,0),
+    Array(0,0,0,0,0,1,0,1,1,0),
+    Array(0,1,1,1,1,1,0,0,0,0),
+    Array(0,1,0,0,0,1,1,0,1,0),
+    Array(0,1,0,1,0,1,0,0,1,0),
+    Array(0,0,0,1,0,0,0,0,1,0)
   )
 
   val offsets = Array((-1, 0), (1, 0), (0, -1), (0, 1))
   def breadthFirstShortestPath(sx: Int, sy: Int, ex: Int, ey: Int): Int = {
-    val queue = new ArrayQueue[(Inc, Int, Int)]()
+    val queue = new ArrayQueue[(Int, Int, Int)]()
     val visited = mutable.Set[(Int, Int)]((sx, sy))
     queue.enqueue((sx, sy, 0))
     while (!queue.isEmpty) {
       val (x, y, steps) = queue.dequeue()
       for ((offsetx, offsety) <- offsets) {
-        val nx = x + offsetxval
-        val ny = y + offsetyval
+        val nx = x + offsetx
+        val ny = y + offsety
         if (nx == ex && ny == ey) return steps + 1
-        if (nx >= 0 && nx < maze.length && ny >= 0 && ny < maze(nx).length && maze(nx)(ny) == 0  && !visited((nx, ny))) {
+        if (nx >= 0 && nx < maze.length && ny >= 0 && ny < maze(nx).length &&
+            maze(nx)(ny) == 0 && !visited((nx, ny))) {
           queue.enqueue((nx, ny, steps + 1))
           visited += nx -> ny
         }
@@ -44,6 +39,6 @@ object shortestPath {
   }
 
   def main(args: Array[String]): Unit = {
-    println(breadthFirstshortestPath(0, 0, 9, 9))
+    println(breadthFirstShortestPath(0, 0, 9, 9))
   }
 }
